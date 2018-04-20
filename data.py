172d7c40a81aca36736f5b2504c2e25021af8b5a
Nick Eu <nickeu@Nicks-MacBook-Air.local>
2018-04-20 21:29:27 +0100

    Added articles

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+    {
+    'id' : 1,
+    'title' : 'Article One',
+    'body' : 'Nunc ut placerat tellus enim, viverra odio etiam cursus eget dictum mauris. Neque aliquam et quam sed neque, curabitur varius, praesent eget neque amet dolor dolor. Nec vestibulum gravida, vitae cras, aenean commodo. Ut nunc blandit, interdum lorem placerat, sit tincidunt phasellus. Sed ac vitae turpis magnis eligendi, ac sed et eget proin a, viverra purus ante ornare vitae. Integer lacus enim wisi, hac aliquam conubia habitasse ipsum turpis, sollicitudin nulla mauris nulla ante, mus nullam et justo.',
+    'author' : 'Me',
+    'create_date' : '20/04/2018'
+    },
+
+    {
+    'id' : 2,
+    'title' : 'Article Two ',
+    'body' : 'Nunc ut placerat tellus enim, viverra odio etiam cursus eget dictum mauris. Neque aliquam et quam sed neque, curabitur varius, praesent eget neque amet dolor dolor. Nec vestibulum gravida, vitae cras, aenean commodo. Ut nunc blandit, interdum lorem placerat, sit tincidunt phasellus. Sed ac vitae turpis magnis eligendi, ac sed et eget proin a, viverra purus ante ornare vitae. Integer lacus enim wisi, hac aliquam conubia habitasse ipsum turpis, sollicitudin nulla mauris nulla ante, mus nullam et justo.',
+    'author' : 'Me',
+    'create_date' : '20/04/2018'
+    },
+
+    {
+    'id' : 3,
+    'title' : 'Article Three',
+    'body' : 'Nunc ut placerat tellus enim, viverra odio etiam cursus eget dictum mauris. Neque aliquam et quam sed neque, curabitur varius, praesent eget neque amet dolor dolor. Nec vestibulum gravida, vitae cras, aenean commodo. Ut nunc blandit, interdum lorem placerat, sit tincidunt phasellus. Sed ac vitae turpis magnis eligendi, ac sed et eget proin a, viverra purus ante ornare vitae. Integer lacus enim wisi, hac aliquam conubia habitasse ipsum turpis, sollicitudin nulla mauris nulla ante, mus nullam et justo.',
+    'author' : 'Me',
+    'create_date' : '20/04/2018'
+    }
+
+    ]
+
+    return articles
